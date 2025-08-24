9ccfbdff8f3689ab70f658920ce634d84d40c333
Sujalofficial07 <sujalbaloda32@gmail.com>
2025-08-24 10:54:53 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,13 @@
 // ✅ Paste your Firebase config here
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXXXX",
-  appId: "XXXXXX"
+  apiKey: "AIzaSyCpZxMl8oqttPIuEg5wjITJTJ8-swtF0xc",
+  authDomain: "sg-website-a3bf4.firebaseapp.com",
+  projectId: "sg-website-a3bf4",
+  storageBucket: "sg-website-a3bf4.firebasestorage.app",
+  messagingSenderId: "620775585240",
+  appId: "1:620775585240:web:d98c3f97a92d1ea4cf6ead",
+  measurementId: "G-DGCMH09V7T"
 };
 
 // Initialize Firebase
